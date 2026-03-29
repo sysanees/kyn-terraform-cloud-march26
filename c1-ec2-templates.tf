300143f9f2a23ad04402de9901a6c0b2077be48c
anees <sysanees@yahoo.com>
2026-03-29 07:11:34 +0000

    change the template version

--- c1-ec2-templates.tf
+++ c1-ec2-templates.tf
@@ -2,6 +2,7 @@ resource "aws_launch_template" "ec2_instance" {
   name_prefix   = "anees-web-launch-template"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type["dev"]
+  update_default_version = true #we are upding the default version
 
   key_name               = "Anees_Key"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
@@ -10,7 +11,7 @@ resource "aws_launch_template" "ec2_instance" {
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "Hello from Auto Scaling" > /var/www/html/index.html
+echo "Hello from Auto Scaling $HOSTNAME" > /var/www/html/index.html
 EOF
 )
 }
--- c3.autoscaling.tf
+++ c3.autoscaling.tf
@@ -1,13 +1,31 @@
-resource "aws_autoscaling_policy" "cpu_tracking_policy" {
-  name                   = "anees-cpu-tracking"
-  autoscaling_group_name = aws_autoscaling_group.web.name #this policy need to added to autoscaling group
-  policy_type            = "TargetTrackingScaling" #this is the type of policy that we want to create
-
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
+resource "aws_autoscaling_group" "web" {
+  desired_capacity = 2
+  max_size         = 4
+  min_size = 2 #it should create two instances at the time of creation of autoscaling group and it can scale up to 4 instances and it will not scale down below 2 instances
+  name              = "anees-autoscaling-group"
+  #i want that the instance should be deployed in two az 
+  vpc_zone_identifier = values(aws_subnet.sky_subnet)[*].id #this is the subnet where the autoscaling group will be created
+  target_group_arns = [ aws_lb_target_group.app_tg.arn ]
+  health_check_type = "ELB" #this is the health check type for the autoscaling group
+    launch_template {
+        id      = aws_launch_template.ec2_instance.id 
+        version = aws_launch_template.ec2_instance.latest_version
     }
-    target_value           = 10.0 # Target 50% CPU .5 millicores of CPU utilization. When the average CPU utilization exceeds this value, the policy will trigger scaling actions.
-    
+
+  instance_refresh {
+    strategy = "Rolling"
+
+    preferences {
+      min_healthy_percentage = 50
+    }
+
+    triggers = ["launch_template"]
+  }
+
+   tag {
+     key                 = "Name"
+     value               = "anees-autoscaling-group"
+     propagate_at_launch = true
+   
   }
 }
